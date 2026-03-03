94746b639cb179488358edda127af0759fb59498
Adam Bowker <adam.b@posthog.com>
2026-03-03 13:06:12 -0800

    feat(surveys): add event property filters to guided editor

--- frontend/src/scenes/surveys/Survey.tsx
+++ frontend/src/scenes/surveys/Survey.tsx
@@ -204,7 +204,7 @@ export function SurveyDisplaySummary({
                     </div>
                 </div>
             )}
-            {survey.appearance?.surveyPopupDelaySeconds && (
+            {!!survey.appearance?.surveyPopupDelaySeconds && (
                 <div className="flex flex-col font-medium gap-1">
                     <div className="flex-row">
                         <span>Delay before showing:</span>{' '}
